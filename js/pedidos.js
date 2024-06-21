70ad530de4985a690405a716c480eab28990409c
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2024-06-20 23:06:37 -0300

    Marcelo W. Castellan

--- js/pedidos.js
+++ js/pedidos.js
@@ -7,7 +7,17 @@ const cargarPedidos = async () => {
     let respuesta = await fetch("https://tpo-nodejs-bb.vercel.app/pedidos");
     if (respuesta.status == 200) {
       let datos = await respuesta.json();
-      let contenedor = `<table class="Pedidos">`;
+      let contenedor = `<table class="Pedidos">
+                       <p>PEDIDOS</p>
+                       <th class="itemp">id</th>
+                       <th class="itemp">FECHA_COMPRA</th>
+                       <th class="itemp">IDCLIENTE</th>
+                       <th class="itemp">IDPRODUCTO</th>
+                       <th class="itemp">CANTIDAD</th>
+                       <th class="itemp">PRECIO</th>
+                       <th class="itemp">IMPORTE</th>
+                       <th class="itemp">IDESTADO</th>
+                       `;
       datos.forEach((pedido) => {
         // Pedidos
         contenedor += `<tr class="Pedido">
@@ -19,6 +29,7 @@ const cargarPedidos = async () => {
                        <td class="itemp precio">${pedido.PRECIO}</td>
                        <td class="itemp importe">${pedido.IMPORTE}</td>
                        <td class="itemp estado">${pedido.IDESTADO}</td>
+
                        </tr>    `;
       });
       contenedor += `</table">`;
