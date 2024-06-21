159d94f906575f9493ee2168da578e5264ac313f
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2024-06-21 01:21:11 -0300

    Marcelo W. Castellan

--- js/pedidos.js
+++ js/pedidos.js
@@ -8,7 +8,7 @@ const cargarPedidos = async () => {
     if (respuesta.status == 200) {
       let datos = await respuesta.json();
       let contenedor = `<table class="Pedidos">
-                       <p>PEDIDOS</p>
+                       <p><h2>PEDIDOS</h2></p>
                        <th class="itemp">id</th>
                        <th class="itemp">FECHA_COMPRA</th>
                        <th class="itemp">IDCLIENTE</th>
@@ -20,18 +20,40 @@ const cargarPedidos = async () => {
                        `;
       datos.forEach((pedido) => {
         // Pedidos
-        contenedor += `<tr class="Pedido">
+        contenedor += `<tr class="Pedido"> 
                        <td class="itemp id">${pedido.id}</td>
                        <td class="itemp fecha_compra">${pedido.FECHA_COMPRA}</td>
                        <td class="itemp idcliente">${pedido.IDCLIENTE}</td>
-                       <td class="itemp idproducto">${pedido.IDPRODUCTO}</td>
-                       <td class="itemp cantidad">${pedido.CANTIDAD}</td>
-                       <td class="itemp precio">${pedido.PRECIO}</td>
-                       <td class="itemp importe">${pedido.IMPORTE}</td>
+                       <td class="itemp idproducto">
+                          <input type="number" name="idproducto" value=${pedido.IDPRODUCTO}></td>
+                       <td class="itemp cantidad">
+                          <input type="number" name="cantidad" value=${pedido.CANTIDAD}></td>
+                       <td class="itemp precio">
+                          <input type="number" name="precio" value=${pedido.PRECIO}></td>
+                       <td class="itemp importe">
+                          <input type="number" name="importe" value=${pedido.IMPORTE}></td>
                        <td class="itemp estado">${pedido.IDESTADO}</td>
-                       
+                       <td class="itemp"><input type="submit" value="Modificar"></td>
+                       <td class="itemp"><input type="submit" value="Eliminar"></td>
                        </tr>    `;
       });
+      contenedor += `<tr class="Pedido"> 
+                       <td class="itemp id"></td>
+                       <td class="itemp fecha_compra">
+                          <input type="date" name="fecha_compra" required></td>
+                       <td class="itemp idcliente">
+                          <input type="number" name="idcliente" value=0 required></td>
+                       <td class="itemp idproducto">
+                          <input type="number" name="idproducto" value=0 required></td>
+                       <td class="itemp cantidad">
+                          <input type="number" name="cantidad" value=0 required></td>
+                       <td class="itemp precio">
+                          <input type="number" name="precio" value=0 required></td>
+                       <td class="itemp importe">
+                          <input type="number" name="importe" value=0 required></td>
+                       <td class="itemp estado">1</td>
+                       <td class="itemp"><input type="submit" value="Agregar"></td>
+                      </tr>    `;
       contenedor += `</table">`;
       document.querySelector(".Contenedor").innerHTML = contenedor;
     } else if (respuesta.status === 404) {
